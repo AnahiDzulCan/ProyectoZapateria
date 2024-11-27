51b743e36b8b3ea2789a637e8957305cc274a216
AnahiDzulCan <anahidcan@gmail.com>
2024-11-26 22:34:47 -0600

    Version 3 - 26/11/2024
    Modifique index. 
    Falta form de agregar productos y modificar. terminar footer, revisar cambios del css. 
    Poner  "animaciones" en el css.

--- Js.js
+++ Js.js
@@ -24,3 +24,22 @@ typewriter.typeString('La mejor zapateria')
       toast.classList.remove('show'); // La oculta después de 3 segundos
     }, 3000);
   }
+
+// Función para iniciar sesión de inicioasesion
+ // Función para verificar que las contraseñas coincidan
+ function verificarContraseñas() {
+            // Obtener los valores de los campos de contraseña y confirmación
+     var password = document.getElementById("password").value;
+     var repassword = document.getElementById("repassword").value;
+    
+            // Verificar si las contraseñas coinciden
+      if (password !== repassword) {
+         alert("Las contraseñas no coinciden. Por favor, intente de nuevo.");
+          return false; // Impide el envío del formulario
+          }
+    
+        return true; // Permite el envío del formulario
+  }
+    
+  // Asociar la función al evento de envío del formulario
+  document.querySelector("form").onsubmit = verificarContraseñas;
